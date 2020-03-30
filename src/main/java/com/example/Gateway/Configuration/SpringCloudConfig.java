f7b56404dabc49509176918bd12a7f58fb33b379
sravaisridhara <sravanisridhara94@gmail.com>
2020-03-30 13:06:41 -0400

    Initial commit

---
+++ src/main/java/com/example/Gateway/Configuration/SpringCloudConfig.java
@@ -0,0 +1,20 @@
+package com.example.Gateway.Configuration;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringCloudConfig {
+
+    @Bean
+    public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(r -> r.path("/accounts/**")
+                        .uri("lb://config-client")
+                        .id("accountsModule"))
+                .build();
+    }
+
+}
