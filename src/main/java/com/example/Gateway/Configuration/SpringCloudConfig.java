06c98f6f35bf38bc7d4fa4ecff1695fc3f7ede1e
sravaisridhara <sravanisridhara94@gmail.com>
2020-04-01 15:22:32 -0400

    Added hystrix dashboard.

--- src/main/java/com/example/Gateway/Configuration/SpringCloudConfig.java
+++ src/main/java/com/example/Gateway/Configuration/SpringCloudConfig.java
@@ -2,9 +2,11 @@ package com.example.Gateway.Configuration;
 
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@EnableHystrix
 @Configuration
 public class SpringCloudConfig {
 
@@ -12,6 +14,8 @@ public class SpringCloudConfig {
     public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
         return builder.routes()
                 .route(r -> r.path("/v1/accounts/**")
+                        .filters(f -> f.hystrix(h -> h.setName("account-service")
+                                .setFallbackUri("forward:/fallback/accountsFallback")))
                         .uri("lb://account-service")
                         .id("accountsModule"))
                 .build();
---
+++ src/main/java/com/example/Gateway/FallBacks/GatewayFallback.java
@@ -0,0 +1,14 @@
+package com.example.Gateway.FallBacks;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/fallback")
+public class GatewayFallback {
+
+    @RequestMapping("/accountsFallback")
+    public String accountsFallback(){
+        return "Account-Service is Down. Please try again later.";
+    }
+}
